6f33e8b14fe3f7196429db83035081951ebf9e14
eoneoff <eoneoff@gmail.com>
2018-01-28 23:06:49 +0200

    second hometask widthout transactions and async

---
+++ hometask2/BankService/BankService/IBankService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.Runtime.Serialization;
+
+namespace BankService
+{
+    [ServiceContract]
+    interface IBankService
+    {
+        [OperationContract]
+        void Create(int accountNo);
+
+        [OperationContract]
+        void Change(int accountNo, decimal sum);
+
+        [OperationContract]
+        void Delete(int accountNo);
+
+        [OperationContract]
+        decimal Balance(int accountNo);
+
+        [OperationContract]
+        IEnumerable<TransactionData> Movement(int accountNo);
+    }
+
+    [DataContract]
+    public struct TransactionData
+    {
+        [DataMember]
+        public DateTime transDate;
+
+        [DataMember]
+        public decimal transAmount;
+    }
+}
---
+++ hometask2/BankService/BankService/Service.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankService
+{
+    class Service : IBankService
+    {
+        public decimal Balance(int accountNo)
+        {
+            using (AccountEntities db = new AccountEntities())
+            {
+                if(db.accounts.Any(a=>a.account==accountNo))
+                {
+                    return db.accounts.Find(accountNo).balance;
+                }
+                else
+                    throw new ArgumentOutOfRangeException($"Счет № {accountNo} отсутствует в базе");
+            }
+        }
+
+        public void Change(int accountNo, decimal sum)
+        {
+            using (AccountEntities db = new AccountEntities())
+            {
+                if (db.accounts.Any(a => a.account == accountNo))
+                {
+                    accounts account = db.accounts.Find(accountNo);
+                    transactions transaction = new transactions { accounts = account, transAmount = sum, transDate = DateTime.Now };
+                    db.transactions.Add(transaction);
+                    db.SaveChanges();
+                }
+                else
+                    throw new ArgumentOutOfRangeException($"Счет № {accountNo} отсутствует в базе");
+            }
+        }
+
+        public void Create(int accountNo)
+        {
+            using (AccountEntities db = new AccountEntities())
+            {
+                if (db.accounts.Any(a => a.account == accountNo))
+                {
+                    accounts account = new accounts { account = accountNo, balance = 0 };
+                    db.accounts.Add(account);
+                    db.SaveChanges();
+                }
+                else
+                    throw new ArgumentOutOfRangeException($"Счет № {accountNo} отсутствует в базе");
+            }
+        }
+
+        public void Delete(int accountNo)
+        {
+            using (AccountEntities db = new AccountEntities())
+            {
+                if (db.accounts.Any(a => a.account == accountNo))
+                {
+                    accounts account = db.accounts.Find(accountNo);
+                    db.accounts.Remove(account);
+                    db.SaveChanges();
+                }
+                else
+                    throw new ArgumentOutOfRangeException($"Счет № {accountNo} отсутствует в базе");
+            }
+        }
+
+        public IEnumerable<TransactionData> Movement(int accountNo)
+        {
+            using (AccountEntities db = new AccountEntities())
+            {
+                if (db.accounts.Any(a => a.account == accountNo))
+                {
+                    var trans = db.transactions.Where(t => t.id == accountNo).OrderBy(t => t.transDate).Select(t => new TransactionData { transDate = t.transDate, transAmount = t.transAmount }).ToList();
+                    return trans;
+      
+                }
+                else
+                    throw new ArgumentOutOfRangeException($"Счет № {accountNo} отсутствует в базе");
+            }
+        }
+    }
+}
