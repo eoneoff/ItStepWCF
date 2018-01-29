37368116f0585ed285a04992357818bf7d9876b1
eoneoff <eoneoff@gmail.com>
2018-01-29 22:31:47 +0200

    console server

--- hometask2/BankService/BankService/Program.cs
+++ hometask2/BankService/BankService/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ServiceModel;
 
 namespace BankService
 {
@@ -10,6 +11,24 @@ namespace BankService
     {
         static void Main(string[] args)
         {
+            Console.Title = "Bank Server";
+            ServiceHost sh = new ServiceHost(typeof(Service));
+            try
+            {
+                sh.Open();
+                Console.WriteLine("Bank Server Online...");
+                Console.WriteLine("Press <ENTER> to shutdown");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Bank Server down with error {ex.Message} at {DateTime.Now}");
+            }
+            finally
+            {
+                sh.Close();
+                Console.WriteLine("Bank Server down correctly");
+            }
         }
     }
 }
